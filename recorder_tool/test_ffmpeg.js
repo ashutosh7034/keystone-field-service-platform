704e2b41cbb604dce0a8b826cb27cb62c712d6d4
ashutosh7034 <ashutosh3276s16@gmail.com>
2026-09-20 00:12:30 +0530

    chore: finalize local testing and cleanup

--- recorder_tool/test_ffmpeg.js
+++
@@ -1,5 +0,0 @@
-const ffmpeg = require('@ffmpeg-installer/ffmpeg');
-const { execSync } = require('child_process');
-
-console.log('FFMPEG PATH:', ffmpeg.path);
-console.log('VERSION:', execSync(`"${ffmpeg.path}" -version`).toString().split('\n')[0]);
